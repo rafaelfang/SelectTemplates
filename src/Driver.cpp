cd9c3c8d555e3866491b3d93b26fd6607ac2a39c
rafaelfang <cf797@mail.missouri.edu>
2015-09-09 19:07:17 -0500

    Driver

---
+++ src/Driver.cpp
@@ -0,0 +1,40 @@
+//============================================================================
+// Name        : Utility.cpp
+// Author      : 
+// Version     :
+// Copyright   : Your copyright notice
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <cstring>
+#include <iostream>
+
+
+#include "BLAPDBImpl.h"
+
+
+using namespace std;
+
+int main(int argc, char* argv[]) {
+	if (argc != 3) {
+		cout << "not enough arguments" << endl;
+		cout << "<executable> <type> <proteinName>" << endl;
+
+	}
+
+	// -blaPDB T0837
+	if (strcmp(argv[1], "-blaPDB") == 0) {
+		BLAPDBImpl blaPDBImpl(argv[2]);
+		blaPDBImpl.populateResultVector();
+		blaPDBImpl.makeDirectory();
+		blaPDBImpl.write2Json();
+		blaPDBImpl.setup3DCoords();
+		blaPDBImpl.calculateDistanceMatrix();
+
+	}
+
+
+
+
+	return 0;
+}
