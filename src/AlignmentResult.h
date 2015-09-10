43038d71a920c8e37309c4a18f3af85335308e86
rafaelfang <cf797@mail.missouri.edu>
2015-09-09 19:06:47 -0500

    AlignmentResult.h

---
+++ src/AlignmentResult.h
@@ -0,0 +1,41 @@
+/*
+ * AlignmentResult.h
+ *
+ *  Created on: Apr 21, 2015
+ *      Author: cf797
+ */
+
+#ifndef ALIGNMENTRESULT_H_
+#define ALIGNMENTRESULT_H_
+
+#include <string>
+#include <vector>
+
+using namespace std;
+class AlignmentResult {
+public:
+	AlignmentResult();
+
+	virtual ~AlignmentResult();
+	vector<float>& getXCoords();
+	void setXCoords(vector<float>& coords);
+	vector<float>& getYCoords();
+	void setYCoords(vector<float>& coords);
+	vector<float>& getZCoords();
+	void setZCoords(vector<float>& coords);
+	vector<char>& getTemplateSeq();
+	void setTemplateSeq(vector<char>& templateSeq);
+	vector<vector<float> >& getDistMat();
+	void setDistMat(vector<vector<float> >& distMat);
+
+protected:
+
+	vector<float> XCoords;
+	vector<float> YCoords;
+	vector<float> ZCoords;
+	vector<char> templateSeq;
+
+	vector<vector<float> > distMat;
+};
+
+#endif /* ALIGNMENTRESULT_H_ */
