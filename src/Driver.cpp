5d544c10b504b676ba88230aed72260360d341fe
rafaelfang <cf797@mail.missouri.edu>
2015-09-14 09:58:42 -0500

    driver

--- src/Driver.cpp
+++ src/Driver.cpp
@@ -29,8 +29,8 @@ int main(int argc, char* argv[]) {
 		blaPDBImpl.makeDirectory();
 		blaPDBImpl.write2Json();
 		blaPDBImpl.setup3DCoords();
-		blaPDBImpl.calculateDistanceMatrix();
-		//blaPDBImpl.calculateSimilarityMatrix();
+		//blaPDBImpl.calculateDistanceMatrix();
+		blaPDBImpl.calculateSimilarityMatrix();
 	}
 
 
